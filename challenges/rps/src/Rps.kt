3bb53878c3024bf62e6b30b6b2ae9d8e3ca5f3ac
eiselems <marcus.eisele@gmail.com>
2020-04-10 00:58:35 +0200

    Updated Rock Paper Scissors with Solution

--- challenges/rps/src/Rps.kt
+++ challenges/rps/src/Rps.kt
@@ -1,45 +1,55 @@
+import Choice.*
 import kotlin.random.Random
 
 fun main() {
+
     var userWins = 0
-    var computerWins = 0
+    var enemyWins = 0
     var draws = 0
-    while (userWins + computerWins + draws < 10) {
-        println("Choose (r)ock, (p)aper, or (s)cissor")
-        val userChoice = readLine().orEmpty()
+
+    while (userWins + enemyWins < 10 || userWins == enemyWins) {
+
+        println("Choose (r)ock, (p)aper, or (s)cissors:")
+        val userChoice = when (readLine().orEmpty()) {
+            "r" -> ROCK
+            "p" -> PAPER
+            "s" -> SCISSORS
+            else -> UNDEFINED
+        }
 
         val enemyChoice = when (Random.nextInt(0, 3)) {
-            0 -> "r"
-            1 -> "p"
-            2 -> "s"
-            else -> "should not happen"
+            0 -> ROCK
+            1 -> PAPER
+            2 -> SCISSORS
+            else -> UNDEFINED
         }
 
         val outcome = if (enemyChoice == userChoice) {
             draws++
             Outcome.DRAW
         } else if (
-            (enemyChoice == "r" && userChoice == "p") ||
-            (enemyChoice == "p" && userChoice == "s") ||
-            (enemyChoice == "s" && userChoice == "r")
+            (userChoice == ROCK && enemyChoice == SCISSORS) ||
+            (userChoice == PAPER && enemyChoice == ROCK) ||
+            (userChoice == SCISSORS && enemyChoice == PAPER)
         ) {
             userWins++
-            Outcome.VICTORY
+            Outcome.WIN
         } else {
-            computerWins++
+            enemyWins++
             Outcome.LOSS
         }
 
-        println("Enemy choose $enemyChoice")
-        println("Outcome was: $outcome")
-        println("--------------")
+        println("userChoice: $userChoice and enemyChoice was $enemyChoice. The outcome was a $outcome")
+        println("Current Score: $userWins vs $enemyWins")
     }
 
-    println("--------------")
-    println("Total scores are:")
-    println("User: $userWins, Computer: $computerWins, Draws: $draws")
+    println("Game is over! Final score was: You ($userWins) vs Computer ($enemyWins) and $draws draws")
+}
+
+enum class Choice {
+    ROCK, PAPER, SCISSORS, UNDEFINED
 }
 
 enum class Outcome {
-    VICTORY, DRAW, LOSS
+    WIN, DRAW, LOSS
 }
